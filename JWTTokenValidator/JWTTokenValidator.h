5f4f03702ac366f38346c9d81f23e5d771206485
Thomas Leonhardt <t.leonhardt@gmx.net>
2024-05-10 11:31:22 +0200

    Initial Commit

---
+++ JWTTokenValidator/JWTTokenValidator.h
@@ -0,0 +1,18 @@
+//
+//  JWTTokenValidator.h
+//  JWTTokenValidator
+//
+//  Created by Thomas (privat) Leonhardt on 10.05.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JWTTokenValidator.
+FOUNDATION_EXPORT double JWTTokenValidatorVersionNumber;
+
+//! Project version string for JWTTokenValidator.
+FOUNDATION_EXPORT const unsigned char JWTTokenValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWTTokenValidator/PublicHeader.h>
+
+
